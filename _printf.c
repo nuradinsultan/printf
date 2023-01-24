3519c9b7a5558b0efe12aa179e90a4da7c0a285d
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-01-24 17:51:20 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * _printf - is a function that selects the correct function to print.
  * @format: identifier to look for.
@@ -7,12 +7,20 @@
 int _printf(const char * const format, ...)
 {
 	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char},
+		{"%s", printf_string}, 
+                {"%c", printf_char},
 		{"%%", printf_37},
-		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
+		{"%i", printf_int}, 
+                {"%d", printf_dec}, 
+                {"%r", printf_srev},
+		{"%R", printf_rot13}, 
+                {"%b", printf_bin}, 
+                {"%u", printf_unsigned},
+		{"%o", printf_oct}, 
+                {"%x", printf_hex}, 
+                {"%X", printf_HEX},
+		{"%S", printf_exclusive_string},
+                {"%p", printf_pointer}
 	};
 
 	va_list args;
