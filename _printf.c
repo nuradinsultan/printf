a6e701f7e8a3656229bbb32eaea797c73ccd9ea6
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-01-24 23:05:30 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,43 +1,39 @@
+#include <stdio.h>
+#include <stdarg.h>
 #include "main.h"
+
 /**
- * _printf - printf function
- * @format: const char pointer
- * Return: b_len
+ * _printf - Function that produces output according to a format.
+ * @format: Pointer
+ * Return: Always 0
  */
+
 int _printf(const char *format, ...)
 {
-	int (*pfunc)(va_list, flags_t *);
-	const char *p;
-	va_list arguments;
-	flags_t flags = {0, 0, 0};
+	va_list print;
+	int count = 0;
 
-	register int count = 0;
+	MyPrint ops[] = {
+		{"c", op_character},
+		{"s", op_string},
+		{"i", op_integer},
+		{"d", op_integer},
+		{"r", op_reverse},
+		{"R", op_rot13},
+		{"b", op_binary},
+		{"o", op_octal},
+		{"u", op_unsigned_decimal},
+		{"x", op_hex},
+		{"X", op_HEX},
+		{"S", op_SString},
+		{"p", op_address},
+	};
 
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
+	if (format == NULL)
 		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = format; *p; p++)
-	{
-		if (*p == '%')
-		{
-			p++;
-			if (*p == '%')
-			{
-				count += _putchar('%');
-				continue;
-			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
-	}
-	_putchar(-1);
-	va_end(arguments);
+	va_start(print, format);
+
+	count = validator(format, print, ops);
+	va_end(print);
 	return (count);
 }
